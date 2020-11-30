19f13d2ce6a7c240aaa632e8607af91487c7552f
cwilkins-8 <71436174+cwilkins-8@users.noreply.github.com>
2020-11-30 22:54:17 +0000

    Add files via upload

---
+++ 5.1.py
@@ -0,0 +1,17 @@
+#5.1 Conditional Tests
+animal = 'cat'
+print("Is animal == 'cat'? I predict True.")
+print(animal == 'cat')
+
+print("Is animal == 'dog'? I predict False.")
+print(animal == 'dog')
+
+phone = 'iphone'
+print("Is your phone == 'iphone'? I predict True.")
+print (phone == 'iphone')
+
+
+print("\nIs your phone == 'andriod'? I predict True.")
+print (phone == 'andriod')
+
+
